fba7831c1276159c2140650fc3428741f62ff052
ahmilajriya <m.ahmil.xan2@gmail.com>
2026-05-04 21:20:22 +0700

    Update latihanpt2.java

--- latihanpt2/latihanpt2.java
+++ latihanpt2/latihanpt2.java
@@ -1,1 +1,28 @@
-test
+import java.util.Scanner;
+
+public class latihan2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String nama;
+        String nim;
+        String programStudi;
+
+        System.out.print("Input Nama: ");
+        nama = input.nextLine();
+
+        System.out.print("Input NIM: ");
+        nim = input.nextLine();
+
+        System.out.print("Input Program Studi: ");
+        programStudi = input.nextLine();
+
+        System.out.println("\nData Mahasiswa:");
+        System.out.println("Nama           : " + nama);
+        System.out.println("NIM            : " + nim);
+        System.out.println("Program Studi  : " + programStudi);
+
+        input.close();
+    }
+}
